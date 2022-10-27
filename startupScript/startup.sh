e83a98f2bbe4a15d14dcdcc66c9352612f26d553
George Beckert <georgebeckert@gmail.com>
2022-10-27 15:57:43 -0400

    everforest updates

--- startupScript/startup.sh
+++ startupScript/startup.sh
@@ -6,11 +6,11 @@ cd ~/Repos/lemonade;
 code .; 
 cd ~/Repos/lemonade-api/; 
 code .;
-st -e zsh -c "cd ~/Repos/lemonade; yarn start" & disown;
-st -e zsh -c "cd ~/Repos/lemonade; yarn storybook" & disown;
-st -e zsh -c 'ngrok http localhost:5000 --hostname "george-treasury.ngrok.io" --region us' & disown;
-st -e zsh -c 'ngrok http 127.0.0.1:9000 --hostname=george-webhook.ngrok.io --region us' & disown;
-st -e zsh -c 'ngrok http 127.0.0.1:3000 --hostname "*.mighty.george.ngrok.io" --region us' & disown;
+kitty --detach zsh -c "cd ~/Repos/lemonade; yarn start" & disown;
+kitty --detach zsh -c "cd ~/Repos/lemonade; yarn storybook" & disown;
+kitty --detach zsh -c 'ngrok http localhost:5000 --hostname "george-treasury.ngrok.io" --region us' & disown;
+kitty --detach zsh -c 'ngrok http 127.0.0.1:9000 --hostname=george-webhook.ngrok.io --region us' & disown;
+kitty --detach zsh -c 'ngrok http 127.0.0.1:3000 --hostname "*.mighty.george.ngrok.io" --region us' & disown;
 
 
 docui;
