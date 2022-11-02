7d544061c97d30a03191bbc3e0d5dcbe5b0de4b5
George Beckert <georgebeckert@gmail.com>
2022-11-02 16:57:42 -0400

    autrandr and other stuff

--- startupScript/startup.sh
+++ startupScript/startup.sh
@@ -1,7 +1,3 @@
-docker container start pubsub;
-docker container start lemonade-api-api-1;
-docker container start lemonade-flask_server-1;
-docker container start lemonade-api-treasury-1;
 cd ~/Repos/lemonade; 
 code .; 
 cd ~/Repos/lemonade-api/; 
@@ -13,5 +9,9 @@ kitty --detach zsh -c 'ngrok http 127.0.0.1:9000 --hostname=george-webhook.ngrok
 kitty --detach zsh -c 'ngrok http 127.0.0.1:3000 --hostname "*.mighty.george.ngrok.io" --region us' & disown;
 
 
+docker container start pubsub;
+docker container start lemonade-api-api-1;
+docker container start lemonade-flask_server-1;
+docker container start lemonade-api-treasury-1;
 docui;
 
