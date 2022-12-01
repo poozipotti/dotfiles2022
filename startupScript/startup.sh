fb59589a4793551678bb47956caf70de5b1b09f5
George Beckert <georgebeckert@gmail.com>
2022-12-01 17:08:00 -0500

    little startup work :)

--- startupScript/startup.sh
+++ startupScript/startup.sh
@@ -2,16 +2,16 @@ cd ~/Repos/lemonade;
 code .; 
 cd ~/Repos/lemonade-api/; 
 code .;
-kitty --detach zsh -c "cd ~/Repos/lemonade; yarn start" & disown;
-kitty --detach zsh -c "cd ~/Repos/lemonade; yarn storybook" & disown;
-kitty --detach zsh -c 'ngrok http localhost:5000 --hostname "george-treasury.ngrok.io" --region us' & disown;
-kitty --detach zsh -c 'ngrok http 127.0.0.1:9000 --hostname=george-webhook.ngrok.io --region us' & disown;
-kitty --detach zsh -c 'ngrok http 127.0.0.1:3000 --hostname "*.mighty.george.ngrok.io" --region us' & disown;
+kitty --class="startupprogram" --detach zsh -c "cd ~/Repos/lemonade; yarn start" & disown;
+kitty --class="startupprogram" --detach zsh -c "cd ~/Repos/lemonade; yarn storybook" & disown;
+kitty --class="startupprogram" --detach zsh -c 'ngrok http localhost:5000 --hostname "george-treasury.ngrok.io" --region us' & disown;
+kitty --class="startupprogram" --detach zsh -c 'ngrok http 127.0.0.1:9000 --hostname=george-webhook.ngrok.io --region us' & disown;
+kitty --class="startupprogram" --detach zsh -c 'ngrok http 127.0.0.1:3000 --hostname "*.mighty.george.ngrok.io" --region us' & disown;
+kitty --class="startupprogram" --detach zsh -c 'docui' & disown;
 
 
 docker container start pubsub;
 docker container start lemonade-api-api-1;
 docker container start lemonade-flask_server-1;
 docker container start lemonade-api-treasury-1;
-docui;
 
